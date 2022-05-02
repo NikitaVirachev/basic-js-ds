068edd9742343a9163e1ba81dd8ad07a3443804c
NikitaVirachev <guardsolder@gmail.com>
2022-05-02 17:57:30 +0300

    feat: complete task queue

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+ const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -14,20 +14,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
-
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.headNode = null;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() {
+    return this.headNode;
+  }
+
+  enqueue( value ) {
+    let current = this.headNode;
+
+    if (!current) {
+      this.headNode = new ListNode(value);
+    } else {
+      while (current.next) {
+        current = current.next;
+      }
+  
+      let newNode = new ListNode (value);
+      current.next = newNode;
+    }
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let current = this.headNode;
+    this.headNode = current.next;
+    return current.value;
   }
 }
 
